284d2d59c88524577d26cb9173f7ff7296d9001d
moisesmartinezliranzo <moisesmartinezliranzo@gmail.com>
2021-09-30 11:09:23 -0400

    game logic

--- src/Board.js
+++ src/Board.js
@@ -2,20 +2,61 @@ import React, { useState } from "react";
 import { Square } from "./Square";
 
 export const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  let status = `Next player: ${xIsNext ? "X" : "O"}`;
+
+  const handleClick = (i) => {
+    const newSquares = [...squares];
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? "X" : "O";
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
+
   const renderSquare = (i) => {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   };
 
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
-  const handleClick = (i) => {
-    const newSquares = [...squares];
-    newSquares[i] = "X";
-    setSquares(newSquares);
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
   };
 
+  const winner = calculateWinner(squares);
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
+  }
   return (
     <div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(1)}
         {renderSquare(2)}
--- src/Square.js
+++ src/Square.js
@@ -1,4 +1,3 @@
-import React from "react";
 export const Square = ({ value, onClick }) => {
   return (
     <button className="square" onClick={onClick}>
